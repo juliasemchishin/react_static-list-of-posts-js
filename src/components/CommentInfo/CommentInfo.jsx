cb4485567ff6933169da3556430747967f403306
Yuliia Voroniuk <juliasemchishin1545@gmail.com>
2025-12-04 21:51:15 +0200

    added fix

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,6 +1,6 @@
 export const CommentInfo = ({ comment }) => {
   return (
-    <div key={comment.id} className="CommentInfo">
+    <div className="CommentInfo">
       <div className="CommentInfo__title">
         <strong className="CommentInfo__name">{comment.name}</strong>
 
