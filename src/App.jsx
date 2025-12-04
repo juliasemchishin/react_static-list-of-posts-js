8f8c47dffe459be59cac51eaa3f02afbe914a6d6
Yuliia Voroniuk <juliasemchishin1545@gmail.com>
2025-12-04 21:43:21 +0200

    added solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,104 +1,182 @@
 import './App.scss';
 
-// import postsFromServer from './api/posts.json';
-// import commentsFromServer from './api/comments.json';
-// import usersFromServer from './api/users.json';
+import postsFromServer from './api/posts.json';
+import commentsFromServer from './api/comments.json';
+import usersFromServer from './api/users.json';
+import { PostList } from './components/PostList';
+
+export const posts = postsFromServer.map(post => ({
+  ...post,
+  user: usersFromServer.find(user => user.id === post.userId),
+  comments: commentsFromServer.filter(comment => comment.postId === post.id),
+}));
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
+    <PostList posts={posts} />
 
-    <div className="PostList">
-      <div className="PostInfo">
-        <div className="PostInfo__header">
-          <h3 className="PostInfo__title">qui est esse</h3>
+    {/* {posts.map(post => ( */}
+    {/*  <div className="PostList" key={post.id}> */}
+    {/*    <PostInfo post={post} /> */}
 
-          <p>
-            {' Posted by  '}
+    {/* <div className="PostInfo"> */}
+    {/*  <div className="PostInfo__header"> */}
+    {/*    <h3 className="PostInfo__title">{post.title}</h3> */}
 
-            <a className="UserInfo" href="mailto:Sincere@april.biz">
-              Leanne Graham
-            </a>
-          </p>
-        </div>
+    {/*    <p> */}
+    {/*      {' Posted by  '} */}
+    {/*      <UserInfo user={post.user} /> */}
+    {/*    </p> */}
+    {/*  </div> */}
 
-        <p className="PostInfo__body">
-          est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea
-          dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut
-          reiciendis qui aperiam non debitis possimus qui neque nisi nulla
-        </p>
+    {/*  <p className="PostInfo__body"> */}
+    {/*    deserunt eos nobis asperiores et hic est debitis repellat molestiae */}
+    {/*    optio nihil ratione ut eos beatae quibusdam distinctio maiores earum */}
+    {/*    voluptates et aut adipisci ea maiores voluptas maxime */}
+    {/*  </p> */}
 
-        <hr />
+    {/*  <div className="CommentList"> */}
+    {/*    <div className="CommentInfo"> */}
+    {/*      <div className="CommentInfo__title"> */}
+    {/*        <strong className="CommentInfo__name">pariatur omnis in</strong> */}
 
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      </div>
+    {/*        {' by '} */}
 
-      <div className="PostInfo">
-        <div className="PostInfo__header">
-          <h3 className="PostInfo__title">doloremque illum aliquid sunt</h3>
+    {/*        <a */}
+    {/*          className="CommentInfo__email" */}
+    {/*          href="mailto:Telly_Lynch@karl.co.uk" */}
+    {/*        > */}
+    {/*          Telly_Lynch@karl.co.uk */}
+    {/*        </a> */}
+    {/*      </div> */}
 
-          <p>
-            {' Posted by  '}
+    {/*      <div className="CommentInfo__body"> */}
+    {/*        dolorum voluptas laboriosam quisquam ab totam beatae et aut */}
+    {/*        aliquid optio assumenda voluptas velit itaque quidem voluptatem */}
+    {/*        tempore cupiditate in itaque sit molestiae minus dolores magni */}
+    {/*      </div> */}
+    {/*    </div> */}
 
-            <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-              Patricia Lebsack
-            </a>
-          </p>
-        </div>
+    {/*    <div className="CommentInfo"> */}
+    {/*      <div className="CommentInfo__title"> */}
+    {/*        <strong className="CommentInfo__name"> */}
+    {/*          odio adipisci rerum aut animi */}
+    {/*        </strong> */}
 
-        <p className="PostInfo__body">
-          deserunt eos nobis asperiores et hic est debitis repellat molestiae
-          optio nihil ratione ut eos beatae quibusdam distinctio maiores earum
-          voluptates et aut adipisci ea maiores voluptas maxime
-        </p>
+    {/*        {' by '} */}
 
-        <div className="CommentList">
-          <div className="CommentInfo">
-            <div className="CommentInfo__title">
-              <strong className="CommentInfo__name">pariatur omnis in</strong>
+    {/*        <a */}
+    {/*          className="CommentInfo__email" */}
+    {/*          href="mailto:Nikita@garfield.biz" */}
+    {/*        > */}
+    {/*          Nikita@garfield.biz */}
+    {/*        </a> */}
+    {/*      </div> */}
 
-              {' by '}
+    {/*      <div className="CommentInfo__body"> */}
+    {/*        quia molestiae reprehenderit quasi aspernatur aut expedita */}
+    {/*        occaecati aliquam eveniet laudantium omnis quibusdam delectus */}
+    {/*        saepe quia accusamus maiores nam est cum et ducimus et vero */}
+    {/*        voluptates excepturi deleniti ratione */}
+    {/*      </div> */}
+    {/*    </div> */}
+    {/*  </div> */}
+    {/* </div> */}
+    {/* </div> */}
+    {/* ))} */}
 
-              <a
-                className="CommentInfo__email"
-                href="mailto:Telly_Lynch@karl.co.uk"
-              >
-                Telly_Lynch@karl.co.uk
-              </a>
-            </div>
+    {/* <div className="PostList"> */}
+    {/*  <div className="PostInfo"> */}
+    {/*    <div className="PostInfo__header"> */}
+    {/*      <h3 className="PostInfo__title">qui est esse</h3> */}
 
-            <div className="CommentInfo__body">
-              dolorum voluptas laboriosam quisquam ab totam beatae et aut
-              aliquid optio assumenda voluptas velit itaque quidem voluptatem
-              tempore cupiditate in itaque sit molestiae minus dolores magni
-            </div>
-          </div>
+    {/*      <p> */}
+    {/*        {' Posted by  '} */}
 
-          <div className="CommentInfo">
-            <div className="CommentInfo__title">
-              <strong className="CommentInfo__name">
-                odio adipisci rerum aut animi
-              </strong>
+    {/*        <a className="UserInfo" href="mailto:Sincere@april.biz"> */}
+    {/*          Leanne Graham */}
+    {/*        </a> */}
+    {/*      </p> */}
+    {/*    </div> */}
 
-              {' by '}
+    {/*    <p className="PostInfo__body"> */}
+    {/*      est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea */}
+    {/*      dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut */}
+    {/*      reiciendis qui aperiam non debitis possimus qui neque nisi nulla */}
+    {/*    </p> */}
 
-              <a
-                className="CommentInfo__email"
-                href="mailto:Nikita@garfield.biz"
-              >
-                Nikita@garfield.biz
-              </a>
-            </div>
+    {/*    <hr /> */}
 
-            <div className="CommentInfo__body">
-              quia molestiae reprehenderit quasi aspernatur aut expedita
-              occaecati aliquam eveniet laudantium omnis quibusdam delectus
-              saepe quia accusamus maiores nam est cum et ducimus et vero
-              voluptates excepturi deleniti ratione
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    {/*    <b data-cy="NoCommentsMessage">No comments yet</b> */}
+    {/*  </div> */}
+
+    {/*  <div className="PostInfo"> */}
+    {/*    <div className="PostInfo__header"> */}
+    {/*      <h3 className="PostInfo__title">doloremque illum aliquid sunt</h3> */}
+
+    {/*      <p> */}
+    {/*        {' Posted by  '} */}
+
+    {/*        <a className="UserInfo" href="mailto:Julianne.OConner@kory.org"> */}
+    {/*          Patricia Lebsack */}
+    {/*        </a> */}
+    {/*      </p> */}
+    {/*    </div> */}
+
+    {/*    <p className="PostInfo__body"> */}
+    {/*      deserunt eos nobis asperiores et hic est debitis repellat molestiae */}
+    {/*      optio nihil ratione ut eos beatae quibusdam distinctio maiores earum */}
+    {/*      voluptates et aut adipisci ea maiores voluptas maxime */}
+    {/*    </p> */}
+
+    {/*    <div className="CommentList"> */}
+    {/*      <div className="CommentInfo"> */}
+    {/*        <div className="CommentInfo__title"> */}
+    {/*          <strong className="CommentInfo__name">pariatur omnis in</strong> */}
+
+    {/*          {' by '} */}
+
+    {/*          <a */}
+    {/*            className="CommentInfo__email" */}
+    {/*            href="mailto:Telly_Lynch@karl.co.uk" */}
+    {/*          > */}
+    {/*            Telly_Lynch@karl.co.uk */}
+    {/*          </a> */}
+    {/*        </div> */}
+
+    {/*        <div className="CommentInfo__body"> */}
+    {/*          dolorum voluptas laboriosam quisquam ab totam beatae et aut */}
+    {/*          aliquid optio assumenda voluptas velit itaque quidem voluptatem */}
+    {/*          tempore cupiditate in itaque sit molestiae minus dolores magni */}
+    {/*        </div> */}
+    {/*      </div> */}
+
+    {/*      <div className="CommentInfo"> */}
+    {/*        <div className="CommentInfo__title"> */}
+    {/*          <strong className="CommentInfo__name"> */}
+    {/*            odio adipisci rerum aut animi */}
+    {/*          </strong> */}
+
+    {/*          {' by '} */}
+
+    {/*          <a */}
+    {/*            className="CommentInfo__email" */}
+    {/*            href="mailto:Nikita@garfield.biz" */}
+    {/*          > */}
+    {/*            Nikita@garfield.biz */}
+    {/*          </a> */}
+    {/*        </div> */}
+
+    {/*        <div className="CommentInfo__body"> */}
+    {/*          quia molestiae reprehenderit quasi aspernatur aut expedita */}
+    {/*          occaecati aliquam eveniet laudantium omnis quibusdam delectus */}
+    {/*          saepe quia accusamus maiores nam est cum et ducimus et vero */}
+    {/*          voluptates excepturi deleniti ratione */}
+    {/*        </div> */}
+    {/*      </div> */}
+    {/*    </div> */}
+    {/*  </div> */}
+    {/* </div> */}
   </section>
 );
